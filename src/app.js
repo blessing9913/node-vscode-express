8e108283b9c33d2c7f3708988d039800beb36791
blessing9913 <blessing9913@gmail.com>
2022-05-24 18:59:45 +0900

    이미지 업로드 핸들링

--- src/app.js
+++ src/app.js
@@ -10,6 +10,7 @@ app.use(express.json())
 // Template engine에서 사용가능하도록 Static file 서빙
 // api 호출 시 src/public 하위의 동일한 경로/이름을 가진 파일을 참조하지 않도록 '/public' prefix를 선언해줌
 app.use('/public', express.static('src/public'))
+app.use('/uploads', express.static('uploads'))
 
 // View template engin: pug
 app.set('views', 'src/views')
